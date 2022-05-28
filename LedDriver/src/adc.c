4192e3bb96462a5545193f584400eeecc673058c
Filipe Loureiro <filipemloureiro@ua.pt>
2022-05-28 12:27:36 +0100

    adc.* v 1.1

--- LedDriver/src/adc.c
+++ LedDriver/src/adc.c
@@ -4,4 +4,40 @@
 void adc_init()
 {
 
+      const struct device *adc_dev = NULL;
+      int err=0;
+
+      /* ADC setup: bind and initialize */
+      adc_dev = device_get_binding(DT_LABEL(ADC_NID));
+      if (!adc_dev) 
+      {
+	    printk("ADC device_get_binding() failed\n");
+      } 
+
+      err = adc_channel_setup(adc_dev, &adc_conf);
+      if (err) 
+      {
+	    printk("adc_channel_setup() failed with error code %d\n", err);
+      }
+      
+      /* It is recommended to calibrate the SAADC at least once before use, and whenever the ambient temperature has changed by more than 10 °C */
+      NRF_SAADC->TASKS_CALIBRATEOFFSET = 1;
+      return adc_dev;
+}
+
+/* Takes one sample */
+void sample(struct device *adc_dev)
+{
+	int ret;
+	const struct adc_sequence sequence = {
+		.channels=BIT(ADC_CHANNEL_ID),
+		.buffer=adc_sample_buffer,
+		.buffer_size=sizeof(adc_sample_buffer),
+		.resolution=ADC_RESOLUTION,
+	};
+
+	ret = adc_read(adc_dev, &sequence);
+	if (ret) {
+            printk("adc_read() failed with code %d\n", ret);
+	}	
 }
--- LedDriver/src/adc.h
+++ LedDriver/src/adc.h
@@ -6,21 +6,64 @@
 *
 * \author Filipe Loureiro & Miguel Silva
 * \date 25/05/22
-* \version 2.1
+* \version 1.1
 * \bug No bugs detected
 */
+
+#ifndef ADC_H
+#define ADC_H
 
 #include "base.h"
 
 #define ADC_NID DT_NODELABEL(adc) 
 #define ADC_RESOLUTION 10
+#define BUFFER_SIZE 1
+#define TIMER_INTERVAL_MSEC 1000
+
+// Parameters for adc Struct Configuration
 #define ADC_GAIN ADC_GAIN_1_4
 #define ADC_REFERENCE ADC_REF_VDD_1_4
 #define ADC_ACQUISITION_TIME ADC_ACQ_TIME(ADC_ACQ_TIME_MICROSECONDS, 40)
 #define ADC_CHANNEL_ID 1
+//#define ADC_CHANNEL_INPUT NRF_SAADC_INPUT_AIN1 
 
-#define ADC_CHANNEL_INPUT NRF_SAADC_INPUT_AIN1 
-#define BUFFER_SIZE 1
-#define TIMER_INTERVAL_MSEC 1000
+// ADC channel struct Configuration:
+static const struct adc_channel_cfg adc_conf = {
+	.gain = ADC_GAIN,
+	.reference = ADC_REFERENCE,
+	.acquisition_time = ADC_ACQUISITION_TIME,
+	.channel_id = ADC_CHANNEL_ID,
+	//.input_positive = ADC_CHANNEL_INPUT
+};
 
+// Global Variables:
+//const struct device *adc_dev = NULL;
+static uint16_t adc_sample_buffer[BUFFER_SIZE];
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+//  Public Functions:
+
+/**
+* \brief ADC initialization function
+*
+* Initializes the ADC in order to use it 
+*
+* \date 25/04/22
+* \bug No bugs detected
+*/
 void adc_init();
+
+
+/**
+* \brief ADC read function
+*
+* Read a sample from ADC previosly configurated and return it
+*
+* \param [IN] struct device *adc  -> the pointer for adc struct
+* \date 25/04/22
+* \bug No bugs detected
+*/
+void adc_sample(struct device *adc_dev);
+
+#endif
