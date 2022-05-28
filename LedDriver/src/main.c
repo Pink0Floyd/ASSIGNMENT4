49567ea1fb3bb0c280dd6e5aa24d7e6144ee48ab
Pink0Floyd <miguelss@ua.pt>
2022-05-28 14:08:58 +0100

    main finished but still untested

--- LedDriver/src/main.c
+++ LedDriver/src/main.c
@@ -12,38 +12,61 @@
 #define FILTERING_PRIO 1		///< filtering thread priority
 #define ACTUATING_PRIO 1		///< actuating thread priority
 
-#define SAMPLING_STACK_SIZE 256
-#define FILTERING_STACK_SIZE 256
-#define ACTUATING_STACK_SIZE 256
+#define SAMPLING_STACK_SIZE 256						///< sampling thread stack size
+K_THREAD_STACK_DEFINE(sampling_stack,SAMPLING_STACK_SIZE);		///< sampling thread stack size
+#define FILTERING_STACK_SIZE 256						///< filtering thread stack size
+K_THREAD_STACK_DEFINE(filtering_stack,FILTERING_STACK_SIZE);	///< filtering thread stack size
+#define ACTUATING_STACK_SIZE 256						///< actuating thread stack size
+K_THREAD_STACK_DEFINE(actuating_stack,ACTUATING_STACK_SIZE);	///< actuating thread stack size
 
-K_THREAD_STACK_DEFINE(sampling_stack,SAMPLING_STACK_SIZE);
-K_THREAD_STACK_DEFINE(filtering_stack,FILTERING_STACK_SIZE);
-K_THREAD_STACK_DEFINE(actuating_stack,ACTUATING_STACK_SIZE);
+struct k_thread sampling_data;	///< sampling thread initialisation
+k_tid_t sampling_tid;			///< sampling thread initialisation
+struct k_thread filtering_data;	///< filtering thread initialisation
+k_tid_t filtering_tid;			///< filtering thread initialisation
+struct k_thread actuating_data;	///< actuating thread initialisation
+k_tid_t actuating_tid;			///< actuating thread initialisation
 
-struct k_thread sampling_data;
-struct k_thread filtering_data;
-struct k_thread actuating_data;
+const int64_t sampling_period=1000;		///< sampling thread period
+struct k_sem sem_sample;			///< sample ready semafore
+struct k_sem sem_filtsample;			///< filtered sample ready semafore
 
-k_tid_t sampling_tid;
-k_tid_t filtering_tid;
-k_tid_t actuating_tid;
-
-struct k_sem sem_sample;
-struct k_sem sem_filtsample;
+uint16_t filt_input;
+uint1&_t filt_out;
 
 void sampling(void* A,void* B,void* C)
 {
-	
+	int64_t curr_time=k_uptime_get();
+	int64_t end_time=k_uptime_get()+sampling_period;
+	while(1)
+	{
+		filt_input=adc_sample();
+		k_sem_give(&sem_sample);
+
+		curr_time=k_uptime_get();			// sleep until next sampling period
+		if(curr_time<end_time)				// sleep until next sampling period
+		{							// sleep until next sampling period
+			k_msleep(end_time-curr_time);		// sleep until next sampling period
+		}							// sleep until next sampling period
+	}
 }
 
 void filtering(void* A,void* B,void* C)
 {
-	
+	while(1)
+	{
+		k_sem_take(&sem_sample);
+		filt_out=filter(filt_in);
+		k_sem_give(&sem_filtsample);
+	}
 }
 
 void actuating(void* A,void* B,void* C)
 {
-	
+	while(1)
+	{
+		k_sem_take(&sem_filtsample);
+		pwm_led_set(filt_out);
+	}
 }
 
 void main()
@@ -53,6 +76,9 @@ void main()
 	adc_init();
 	filter_init();
 	pwm_init();
+
+	k_sem_init(&sem_sample, 0, 1);
+	k_sem_init(&sem_filtsample, 0, 1);
 
 	sampling_tid=k_thread_create(&sampling_data,sampling_stack,K_THREAD_STACK_SIZEOF(sampling_stack),
 		sampling,NULL,NULL,NULL,SAMPLING_PRIO,0,K_NO_WAIT);
