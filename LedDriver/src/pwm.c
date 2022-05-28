247c5ba82834fb33f22277d58db595c28d7ac029
Filipe Loureiro <filipemloureiro@ua.pt>
2022-05-28 15:39:59 +0100

    pwm.h v2.2

--- LedDriver/src/pwm.c
+++ LedDriver/src/pwm.c
@@ -3,5 +3,28 @@
 
 void pwm_init()
 {
+      // Initialise GPIO0:
+      gpio0_init();
 
+      // Initialize PWM:
+      pwm0_dev = device_get_binding(DT_LABEL(PWM0_NID));
+      if (pwm0_dev == NULL) 
+      {
+	    printk("Error: Failed to bind to PWM0\n r");
+	    return;
+      }
+      else  
+      {
+	    printk("Bind to PWM0 successful\n\r");            
+      }
+}
+
+void pwm_led_set(uint16_t dutty_cycle)
+{
+      int ret=0;
+      ret = pwm_pin_set_usec(pwm0_dev, LED1, pwmPeriod_us,(unsigned int)(dutty_cycle), PWM_POLARITY_NORMAL);
+      if (ret) {
+                printk("Error %d: failed to set pulse width\n", ret);
+		return;
+            }
 }
--- LedDriver/src/pwm.h
+++ LedDriver/src/pwm.h
@@ -6,10 +6,42 @@
 *
 * \author Filipe Loureiro & Miguel Silva
 * \date 25/05/22
-* \version 2.1
+* \version 2.2
 * \bug No bugs detected
 */
 
-#include "base.h"
+#ifndef PWM_H
+#define PWM_H 
 
+#include "base.h"
+#include "leds.h"
+
+#define PWM0_NID DT_NODELABEL(pwm0) 
+
+// Global Variables:
+
+static const struct device *pwm0_dev;		// Pointer to PWM device structure 
+static unsigned int pwmPeriod_us = 1000;		// PWM priod in us
+
+///////////////////////////////////////////////////////////////////////////////////////
+//  Public Functions:
+
+/**
+* \brief PWM initialization function
+*
+* Initializes the PWM in order to use it 
+*
+* \bug No bugs detected
+*/
 void pwm_init();
+
+/**
+* \brief 
+*
+*\param [IN] uint16_t dutty_cycle                -> Dutty cycle to set the PWM with
+*
+* \bug No bugs detected
+*/
+void pwm_led_set(uint16_t dutty_cycle);
+
+#endif
